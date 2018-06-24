a53b73b4152d77edc71745e19f39944201cac627
Sanachev <40528219+Sanachev@users.noreply.github.com>
2018-06-24 12:20:41 +0300

    Add files via upload

--- move_triangle.py
+++ move_triangle.py
@@ -1,11 +1,24 @@
 import time
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=400, height=200)
+canvas = Canvas(tk, width=400, height=250)
 canvas.pack()
 canvas.create_polygon(10, 10, 10, 60, 50, 35)
-for x in range (0, 60):
-    canvas.move(1, 5, 0)
+def upd():
     tk.update()
     time.sleep(0.05)
+def triangle_move():
+    for x in range (0, 60):
+        canvas.move(1, 5, 0)
+        upd()
+    for x in range (0, 30):
+        canvas.move(1, 0, 5)
+        upd()
+    for x in range (0, 60):
+        canvas.move(1, -5, 0)
+        upd()
+    for x in range (0, 30):
+        canvas.move(1,0, -5)
+        upd()
+triangle_move()
     
