81c7deb8b9aecfc3580f0cd462b60a5a4ab8e0e8
Sanachev <40528219+Sanachev@users.noreply.github.com>
2018-07-21 02:58:05 +0300

    Add files via upload

---
+++ сумма чисел.py
@@ -0,0 +1,26 @@
+#Задача: найти сумму натуральныъ чисел меньших 10000,
+#в которых встречается не менее двух одинаковых цифр
+#пример чисел: 44, 1001, 7712
+
+
+def func():
+    a = 0
+    array = []
+    array2 = []
+    for x in range(10, 10000):
+        array2.append(str(x))
+    for y in array2:
+        if len(y)<3:
+            if y.count(y[0])==2:
+                array.append(y)
+        elif 4>len(y)>2:
+            if y.count(y[0])>=2 or y.count(y[1])>=2:
+                array.append(y)
+        elif len(y)>3:
+            if y.count(y[0])>=2 or y.count(y[1])>=2 or y.count(y[2])>=2:
+                array.append(y)
+    for i in array:
+        a = a + int(i)
+    print(a)
+func()
+            
